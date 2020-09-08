91a64d0a3603c851770b4de73dd7b46a732ff3de
hanisyah <hanisyahadi4@gmail.com>
2020-09-08 14:28:41 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Generate random room name if needed
+// Menghasilkan nama room acak
 if (!location.hash) {
   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
-// Room name needs to be prefixed with 'observable-'
+const drone = new ScaleDrone('nmjY7KLivcQlwOBh');
+// Nama room harus diawali dengan 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
   iceServers: [{
@@ -32,17 +32,17 @@ drone.on('open', error => {
       onError(error);
     }
   });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
+  // Terhubung ke room dan menerima berbagai 'anggota'
+  // terhubung ke room. Server pensinyalan sudah siap.
   room.on('members', members => {
     console.log('MEMBERS', members);
-    // If we are the second user to connect to the room we will be creating the offer
+    // Jika kami adalah pengguna kedua yang terhubung ke room, kami akan membuat penawaran
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
   });
 });
 
-// Send signaling data via Scaledrone
+//Kirim data pensinyalan melalui Scaledrone
 function sendMessage(message) {
   drone.publish({
     room: roomName,
@@ -53,22 +53,22 @@ function sendMessage(message) {
 function startWebRTC(isOfferer) {
   pc = new RTCPeerConnection(configuration);
 
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
+  // 'onicecandidate' memberi tahu kami setiap kali agen ICE perlu mengirimkan a
+  // pesan ke peer lain melalui server pensinyalan
   pc.onicecandidate = event => {
     if (event.candidate) {
       sendMessage({'candidate': event.candidate});
     }
   };
 
-  // If user is offerer let the 'negotiationneeded' event create the offer
+  // Jika pengguna adalah penawar, biarkan acara 'dibutuhkan negosiasi' yang membuat penawaran
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
       pc.createOffer().then(localDescCreated).catch(onError);
     }
   }
 
-  // When a remote stream arrives display it in the #remoteVideo element
+  // Saat aliran jarak jauh tiba, tampilkan di elemen #remoteVideo
   pc.ontrack = event => {
     const stream = event.streams[0];
     if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
@@ -80,29 +80,29 @@ function startWebRTC(isOfferer) {
     audio: true,
     video: true,
   }).then(stream => {
-    // Display your local video in #localVideo element
+    // Tampilkan video lokal Anda dalam elemen #localVideo
     localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
+    // Tambahkan aliran Anda untuk dikirim ke rekan yang terhubung
     stream.getTracks().forEach(track => pc.addTrack(track, stream));
   }, onError);
 
-  // Listen to signaling data from Scaledrone
+  // Dengarkan data pensinyalan dari Scaledrone
   room.on('data', (message, client) => {
-    // Message was sent by us
+    // Pesan telah dikirim oleh kami
     if (client.id === drone.clientId) {
       return;
     }
 
     if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
+      // Ini dipanggil setelah menerima tawaran atau jawaban dari rekan lain
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
+        // Saat menerima tawaran mari kita jawab
         if (pc.remoteDescription.type === 'offer') {
           pc.createAnswer().then(localDescCreated).catch(onError);
         }
       }, onError);
     } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
+      // Tambahkan kandidat ICE baru ke deskripsi jarak jauh koneksi kami
       pc.addIceCandidate(
         new RTCIceCandidate(message.candidate), onSuccess, onError
       );
