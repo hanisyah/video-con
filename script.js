06ba010743de8782abbfece77f6290c8f72ee3ad
hanisyah <hanisyahadi4@gmail.com>
2020-09-16 08:55:18 +0700

    commit chat

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const possibleEmojis = [
+  '🐀','🐁','🐭','🐹','🐂','🐃','🐄','🐮','🐅','🐆','🐯','🐇','🐐','🐑','🐏','🐴',
+  '🐎','🐱','🐈','🐰','🐓','🐔','🐤','🐣','🐥','🐦','🐧','🐘','🐩','🐕','🐷','🐖',
+  '🐗','🐫','🐪','🐶','🐺','🐻','🐨','🐼','🐵','🙈','🙉','🙊','🐒','🐉','🐲','🐊',
+  '🐍','🐢','🐸','🐋','🐳','🐬','🐙','🐟','🐠','🐡','🐚','🐌','🐛','🐜','🐝','🐞',
+];
+function randomEmoji() {
+  var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
+  return possibleEmojis[randomIndex];
+}
+
+const emoji = randomEmoji();
+const name = prompt("What's your name?");
+
 // Menghasilkan nama room acak
 if (!location.hash) {
   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
@@ -8,13 +22,15 @@ const roomHash = location.hash.substring(1);
 const drone = new ScaleDrone('nmjY7KLivcQlwOBh');
 // Nama room harus diawali dengan 'observable-'
 const roomName = 'observable-' + roomHash;
+let room;
+
 const configuration = {
   iceServers: [{
     urls: 'stun:stun.l.google.com:19302'
   }]
 };
-let room;
 let pc;
+let dataChannel;
 
 
 function onSuccess() {};
@@ -29,13 +45,16 @@ drone.on('open', error => {
   room = drone.subscribe(roomName);
   room.on('open', error => {
     if (error) {
-      onError(error);
+      return onError(error);
     }
+    console.log('Connected to signaling server');
   });
   // Terhubung ke room dan menerima berbagai 'anggota'
   // terhubung ke room. Server pensinyalan sudah siap.
   room.on('members', members => {
-    console.log('MEMBERS', members);
+    if (members.length >= 3) {
+      return alert('The room is full');
+    }
     // Jika kami adalah pengguna kedua yang terhubung ke room, kami akan membuat penawaran
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
@@ -51,6 +70,7 @@ function sendMessage(message) {
 }
 
 function startWebRTC(isOfferer) {
+  console.log('Starting WebRTC in as', isOfferer ? 'offerer' : 'waiter');
   pc = new RTCPeerConnection(configuration);
 
   // 'onicecandidate' memberi tahu kami setiap kali agen ICE perlu mengirimkan a
@@ -65,6 +85,14 @@ function startWebRTC(isOfferer) {
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
       pc.createOffer().then(localDescCreated).catch(onError);
+    }
+    dataChannel = pc.createDataChannel('chat');
+    setupDataChannel();
+  }  else {
+    // If user is not the offerer let wait for a data channel
+    pc.ondatachannel = event => {
+      dataChannel = event.channel;
+      setupDataChannel();
     }
   }
 
@@ -108,7 +136,32 @@ function startWebRTC(isOfferer) {
       );
     }
   });
+  startListentingToSignals();
 } 
+
+function startListentingToSignals() {
+  // Listen to signaling data from Scaledrone
+  room.on('data', (message, client) => {
+    // Message was sent by us
+    if (client.id === drone.clientId) {
+      return;
+    }
+    if (message.sdp) {
+      // This is called after receiving an offer or answer from another peer
+      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+        console.log('pc.remoteDescription.type', pc.remoteDescription.type);
+        // When receiving an offer lets answer it
+        if (pc.remoteDescription.type === 'offer') {
+          console.log('Answering offer');
+          pc.createAnswer(localDescCreated, error => console.error(error));
+        }
+      }, error => console.error(error));
+    } else if (message.candidate) {
+      // Add the new ICE candidate to our connections remote description
+      pc.addIceCandidate(new RTCIceCandidate(message.candidate));
+    }
+  });
+}
 
 function localDescCreated(desc) {
   pc.setLocalDescription(
@@ -117,3 +170,60 @@ function localDescCreated(desc) {
     onError
   );
 }
+
+// Hook up data channel event handlers
+function setupDataChannel() {
+  checkDataChannelState();
+  dataChannel.onopen = checkDataChannelState;
+  dataChannel.onclose = checkDataChannelState;
+  dataChannel.onmessage = event =>
+    insertMessageToDOM(JSON.parse(event.data), false)
+}
+
+function checkDataChannelState() {
+  console.log('WebRTC channel state is:', dataChannel.readyState);
+  if (dataChannel.readyState === 'open') {
+    insertMessageToDOM({content: 'WebRTC data channel is now open'});
+  }
+}
+
+function insertMessageToDOM(options, isFromMe) {
+  const template = document.querySelector('template[data-template="message"]');
+  const nameEl = template.content.querySelector('.message__name');
+  if (options.emoji || options.name) {
+    nameEl.innerText = options.emoji + ' ' + options.name;
+  }
+  template.content.querySelector('.message__bubble').innerText = options.content;
+  const clone = document.importNode(template.content, true);
+  const messageEl = clone.querySelector('.message');
+  if (isFromMe) {
+    messageEl.classList.add('message--mine');
+  } else {
+    messageEl.classList.add('message--theirs');
+  }
+
+  const messagesEl = document.querySelector('.messages');
+  messagesEl.appendChild(clone);
+
+  // Scroll to bottom
+  messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', () => {
+  const input = document.querySelector('input[type="text"]');
+  const value = input.value;
+  input.value = '';
+
+  const data = {
+    name,
+    content: value,
+    emoji,
+  };
+
+  dataChannel.send(JSON.stringify(data));
+
+  insertMessageToDOM(data, true);
+});
+
+insertMessageToDOM({content: 'Chat URL is ' + location.href});
